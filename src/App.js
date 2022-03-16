b79c3c8315586d681642ade44dc818d4f85b29bb
Nima sherpa <Toplouie78@gmail.com>
2022-03-16 11:36:14 -0400

    adding filter method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import { render } from "@testing-library/react";
+import { useState } from "react";
 import "./App.css";
 import users from "./data/users.js";
 
 const App = () => {
-	let renderUsers = users.map((user) => {
-		return <li>{user.name}</li>;
+	// State being stored
+	const [searchInput, setSearchInput] = useState("");
+	// Filter users checking if it includes the character ...
+	let filteredUsers = users.filter((user) => {
+		let name = user.name.toLowerCase();
+		return name.includes(searchInput.toLowerCase());
 	});
+	// Mapping the names
+	let renderUsers = filteredUsers.map((user, index) => {
+		return <li key={index}>{user.name}</li>;
+	});
+	const handleSearchInput = (e) => {
+		setSearchInput(e.target.value);
+	};
+
 	return (
 		<div>
 			<h1>User Filter Sort</h1>
+			<form>
+				<label>Search Name</label>
+				<input value={searchInput} onChange={handleSearchInput} />
+			</form>
 			<ul>{renderUsers}</ul>
 		</div>
 	);
